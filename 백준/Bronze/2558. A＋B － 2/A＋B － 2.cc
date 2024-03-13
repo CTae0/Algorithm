7e131f06797945ca1d574604dc557829609415b8
c_tae0 <94282116+CTae0@users.noreply.github.com>
2024-03-13 20:37:54 +0900

    [Bronze V] Title: A+B - 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.cc
@@ -0,0 +1,31 @@
+//
+//  main.cpp
+//  practice
+//
+//  Created by 최태영 on 1/10/24.
+//
+
+#include <iostream>
+#include <set>
+#include <string>
+#include <algorithm>
+
+
+using namespace std;
+
+
+void nandm(int count);
+
+int main(int argc, const char * argv[]) {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int a,b;
+    cin >> a;
+    cin >> b;
+    
+    printf("%d",a+b);
+   
+}
+
+
